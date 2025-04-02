41c38fa52d6c91147a3cb06991b8fee8e8443edb
jdreif01 <jdreif@gamil.com>
2025-04-01 19:11:11 -0700

    Update resource.ts with new staging uri

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,7 +4,7 @@ const environment = process.env.AMPLIFY_ENV || 'dev';
 
 const redirectUris = {
   dev: ['http://localhost:5173/organization-list'],
-  staging: ['https://staging.d31vtilon76l6i.amplifyapp.com/organization-list/'],
+  staging: ['https://staging.d1mjhvpisz36hi.amplifyapp.com/organization-list/'],
   prod: ['https://main.d31vtilon76l6i.amplifyapp.com/organization-list/']
 };
 
